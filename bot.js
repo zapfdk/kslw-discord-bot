e139c70c969f230d7d4be3cd450d9af617df447e
Dominik Zapf <zapfdk@posteo.de>
2019-11-08 19:38:41 +0100

    added maplist feature

--- bot.js
+++ bot.js
@@ -2,22 +2,22 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 let maps = [
-    {id: 0, name: "Luigis Piste", state: none},
-    {id: 1, name: "Peach Beach", state: none},
-    {id: 2, name: "Baby Park", state: none},
-    {id: 3, name: "Staubtrockene Piste", state: none},
-    {id: 4, name: "Pilzbrücke", state: none},
-    {id: 5, name: "Marios Piste", state: none},
-    {id: 6, name: "Daisys Dampfer", state: none},
-    {id: 7, name: "DK Bergland", state: none},
-    {id: 8, name: "Waluigi Arena", state: none},
-    {id: 9, name: "Sorbet Land", state: none},
-    {id: 10, name: "Pilz-City", state: none},
-    {id: 11, name: "DK Bergland", state: none},
-    {id: 12, name: "Wario Colosseum", state: none},
-    {id: 13, name: "Dinodino-Dschungel", state: none},
-    {id: 14, name: "Bowsers Festung", state: none},
-    {id: 15, name: "Regenbogen-Boulevard", state: none}
+    {id: 0, name: "Luigis Piste", state: null},
+    {id: 1, name: "Peach Beach", state: null},
+    {id: 2, name: "Baby Park", state: null},
+    {id: 3, name: "Staubtrockene Piste", state: null},
+    {id: 4, name: "Pilzbrücke", state: null},
+    {id: 5, name: "Marios Piste", state: null},
+    {id: 6, name: "Daisys Dampfer", state: null},
+    {id: 7, name: "DK Bergland", state: null},
+    {id: 8, name: "Waluigi Arena", state: null},
+    {id: 9, name: "Sorbet Land", state: null},
+    {id: 10, name: "Pilz-City", state: null},
+    {id: 11, name: "DK Bergland", state: null},
+    {id: 12, name: "Wario Colosseum", state: null},
+    {id: 13, name: "Dinodino-Dschungel", state: null},
+    {id: 14, name: "Bowsers Festung", state: null},
+    {id: 15, name: "Regenbogen-Boulevard", state: null}
   ];
 
 
